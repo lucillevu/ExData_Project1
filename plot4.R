77f6b210b04eb14dbd1da82927a60adc2ae22316
lucillevu <vtduong285@yahoo.com>
2015-09-11 03:14:03 -0400

    Project1-update plot4

--- plot4.R
+++ plot4.R
@@ -14,7 +14,7 @@ with(dataSelect,{
   plot(Sub_metering_1 ~ Time, type="l", xlab="", ylab="Energy sub metering")
   lines(Sub_metering_2 ~ Time, col="red")
   lines(Sub_metering_3 ~ Time, col="blue")
-  legend("topright", pch = 1, col = c("black","blue", "red"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+  legend("topright", pch = 1, bty="n", col = c("black","blue", "red"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
   #plot graph 4
   plot(Global_reactive_power ~ Time, type="l", xlab="datetime", ylab="Global_reactive_power" )
   
